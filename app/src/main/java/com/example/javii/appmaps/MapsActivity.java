7f05fab564173370932fc6a75399542bfe15662a
Javii <jestevezrial@danielcastelao.org>
2017-02-03 00:53:01 +0100

    Cambios en el metodo para que aparezcan las marcas en los lugares deseados.

--- app/src/main/java/com/example/javii/appmaps/MapsActivity.java
+++ app/src/main/java/com/example/javii/appmaps/MapsActivity.java
@@ -1,17 +1,22 @@
 package com.example.javii.appmaps;
 
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private static final int LOCATION_REQUEST_CODE = 1;
     private GoogleMap mMap;
     private FirstMapFragment mFirstMapFragment;
 
@@ -32,10 +37,45 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        // Controles UI
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        } else {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // Mostrar diálogo explicativo
+            } else {
+                // Solicitar permiso
+                ActivityCompat.requestPermissions(
+                        this,
+                        new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                        LOCATION_REQUEST_CODE);
+            }
+        }
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
+        // Marcadores
+        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)));
+
+        //Añadimos marcadores a cada lugar en el que nos interese poner una marca. Y con el camara position que al inciar la app aparezca el telepizza de al lado del centro.
+        LatLng telepizza = new LatLng(42.236948, -8.712725);
+        googleMap.addMarker(new MarkerOptions()
+                .position(telepizza)
+                .visible(true)
+                .title("Telepizza")
+
+        );
+
+        CameraPosition cameraPosition = CameraPosition.builder()
+                .target(telepizza)
+                .zoom(10)
+                .build();
+
+        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
     }
 }
+
+
